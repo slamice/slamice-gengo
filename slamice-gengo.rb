3e7b4de6e938f37b867490b73b05bc6bf6191710
slamice <slamice@gmail.com>
2012-09-18 01:23:36 +0900

    fixed some styles, added edit page

--- slamice-gengo.rb
+++ slamice-gengo.rb
@@ -3,17 +3,48 @@ require 'sinatra'
 require 'mygengo'
 
 get '/' do
-  mygengo = MyGengo::API.new({
+  @gengo = MyGengo::API.new({
   	:public_key => '5co}B{[1~uyCNwpCy6#N7RV4u)-GW1W9V)FN(li@PW_r9AqM5eOT50jPH8t-=GG]',
   	:private_key => 'A{FohnIPY-|l8L0}b{@-43T|DC3[tb_}9E]t[AJd4X-6bZxT6i5r|BQ7yxCzG$@s',
   	:sandbox => true,
   })
 
-  @stats = mygengo.getAccountStats()
-  @gengo = mygengo.methods
+  def get_jobs()
+    jobs = @gengo.getTranslationJobs()['response']
+  end
   
-  @gengo_firstJob = mygengo.getTranslationJob({:id => 243346})
-  @gengo_secondJob = mygengo.getTranslationJob({:id => 243347})  
+  def get_job(job_entry)
+    @gengo.getTranslationJob({:id => job_entry["job_id"]})['response']['job']
+  end
+  
+  @stats = @gengo.getAccountStats()
+  @jobs = get_jobs();
   
   erb :home
+end
+
+get '/edit' do
+  erb :edit
+end
+
+post '/form' do
+  
+  @gengo = MyGengo::API.new({
+  	:public_key => '5co}B{[1~uyCNwpCy6#N7RV4u)-GW1W9V)FN(li@PW_r9AqM5eOT50jPH8t-=GG]',
+  	:private_key => 'A{FohnIPY-|l8L0}b{@-43T|DC3[tb_}9E]t[AJd4X-6bZxT6i5r|BQ7yxCzG$@s',
+  	:sandbox => true,
+  })
+  
+  puts @gengo.postTranslationJob({
+    :job => {
+      :type => "text",
+      :slug => params[:slug],
+      :lc_src => params[:lc_src],
+      :lc_tgt => params[:lc_tgt],
+      :tier => "standard",
+      :body_src => params[:body_src]
+    }
+  })
+  
+  redirect '/'
 end
